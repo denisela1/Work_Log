cb015e6c53ffe2237a42c59ade0ac3629fb62d95
Denise Kahr <connectdeniz@gmail.com>
2017-09-28 15:20:16 -0700

    Update csv_funcs.py

--- csv_funcs.py
+++ csv_funcs.py
@@ -7,45 +7,94 @@ import re
 def read_dates():
     with open("logs.csv", 'r') as csvfile:
         reader = csv.reader(csvfile)
+        data = []
         i = 0
         for row in reader:
             print(i, row[0], row[1])
             i += 1
-        index = int(input("Enter the number of the date/task as seen on the screen:\n"))
-        data = list(reader)
-        print(data[index])
+        index = int(input("Enter the number of the task number to list the details:\n"))
+        with open("logs.csv", 'r') as csvfile:
+            reader = csv.reader(csvfile)
+            output = list(reader)
+            print((", ".join(output[index])))
+    select = (str(input("To quit, press Q. "
+                            "To go back to the main menu, press any letter.\n"))).upper()
+    if select == "Q":
+        print("You quit.\n")
+    else:
+        menu.main_screen()
 
 
 def read_time():
-    with open("logs.csv", 'r') as csvfile:
-        reader = csv.reader(csvfile)
-        i = 0
-        for row in reader:
-            print(i, row[2])
-            i += 1
-        index = int(input("Enter the number of the task with requested minute spent:\n"))
-        data = list(reader)
-        print(data[index])
+    while True:
+        try:
+            index = str(input("Enter the total amount of minutes spent to list associated tasks:\n"))
+            with open("logs.csv", 'r') as csvfile:
+                reader = csv.reader(csvfile)
+                i = 0
+                results = []
+                for row in reader:
+                    results.append(row)
+                    i += 1
+                    if index in row:
+                        print(i,row)
+                selection = int(input("Select the number of the task (on the left side) you'd like to list the details of.\n"))
+                print(results[selection-1])
+                select = (str(input("To quit, press Q. "
+                            "To go back to the main menu, press any letter.\n"))).upper()
+                if select == "Q":
+                    print("You quit.\n")
+                else:
+                    menu.main_screen()
+        except ValueError:
+            print('Sorry, please enter a valid value')
+        else:
+            break
 
 
 def read_string():
-    with open("logs.csv", 'r') as csvfile:
-        reader = csv.reader(csvfile)
-        keyword = str(input("Enter a keyword to search for a specific task or note.\n"))
-        for row in reader:
-            if keyword.lower() in (row[1]).lower():
-                print("Task including the keyword:\n",row[0], row[1], row[2], row[3])
-        for row in reader:
-            if keyword.lower() in (row[3]).lower():
-                print("Notes including the keyword:\n",row[0], row[1], row[2], row[3])
+    while True:
+        try:
+            keyword = (str(input("Enter a keyword to search for a specific task or note.\n"))).lower()
+            with open("logs.csv", 'r') as csvfile:
+                reader = csv.reader(csvfile)
+                i = 0
+                for row in reader:
+                    if keyword in row[1].lower():
+                        if keyword in row[3].lower():
+                            print("Entries found:")
+                            print(row)
+                            i += 1
+                        else:
+                            print("Entries found:")
+                            print(row)
+                            i += 1
+                if i == 0:
+                    select = (str(input("Sorry, no task and/or note found."
+                                        "To quit, press Q. "
+                                        "To go back to the main menu, press any letter.\n"))).upper()
+                    if select == "Q":
+                        print("You quit.\n")
+                    else:
+                        menu.main_screen()
+        except ValueError:
+            print('Sorry, please enter a valid value')
+        else:
+            break
 
 
 def read_pattern():
-    regexp = str(input("Enter a regular expression to search within existing task names and notes.\n"))
-    with open("logs.csv", 'r') as csvfile:
-        reader = csv.reader(csvfile)
-        match = re.search(regexp, reader)
-        print(match)
+    while True:
+        try:
+            with open("logs.csv", 'r') as csvfile:
+                regexp = ' + str(input("Enter a regular expression to search within existing task names and")) + '
+                reader = csv.reader(csvfile)
+                match = re.findall(regexp, reader)
+                print(match)
+        except ValueError:
+            print('Sorry, please enter a valid value')
+        else:
+            break
 
 
 def writef():
@@ -69,4 +118,3 @@ def writef():
             else:
                 break
         writer.writerow([date_input, task_input, time_input, notes_input])
-        
